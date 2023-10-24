ed011a5c9cba954633db11a73c6a62621059305d
Brenda Barrera <blb686@humboldt.edu>
2023-10-23 20:22:21 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Components/Navbar/Navbar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory/>}/>
+        <Route path='/womens' element={<ShopCategory/>}/>
+        <Route path='/kids' element={<ShopCategory/>}/>
+
+
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
